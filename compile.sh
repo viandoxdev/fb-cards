eabf799203e07f1d78b4dc7d755ce2c7d17ac889
viandoxdev <titouangoulois29@gmail.com>
2025-11-10 02:43:06 +0100

    Added merged source file because why not

--- compile.sh
+++ compile.sh
@@ -1,27 +1,82 @@
 #!/usr/bin/bash
 
+# Turns path of document into a displayable name (just swaps / for - and removes the extension)
 function nameof () {
     dashes="${1//\//-}"
     echo "${dashes%.*}"
 }
 
+# Collect documents:
+#  .typ files that don't contain FLASHBANG IGNORE (explicit) or FLASHBANG INCLUDE (library files not meant to contain cards)
 documents=()
 while IFS=  read -r -d $'\0'; do
     if ! grep -F -q -e '//![FLASHBANG IGNORE]' -e '//![FLASHBANG INCLUDE]' "$REPLY"; then
         documents+=("$REPLY")
     fi
-done < <(find . -name "*.typ" -printf "%P\0")
+done < <(find . -name "*.typ" -not -path "./dist/*" -printf "%P\0")
+# ^ think of excluding dist
 
 mkdir -p ./dist
 
+src="./dist/_full.typ"
+# Regex to recompute paths in the merged file
+re="^(.*)import \"(.+)\.typ\"(.*)$"
+full="./dist/_full.pdf"
+
+# Merged file header
+echo -e "#import \"../cards.typ\": *\n#show: setup" > "$src"
+
+# compile documents, build merged source
 for doc in "${documents[@]}"; do
+
+    # start new section (avoid polluting the global scope)
+    echo "#[" >> "$src"
+
+    # ugly bash line by line write because we need to perform subtle substitutions
+
+    # We skip the begining of the file that is just typst boilerplate to make each document
+    # work on its own
+    append=false
+    while IFS="" read -r p || [[ -n "$p" ]]; do
+        if [ "$append" = true ]; then
+            # check for relative imports
+            if [[ "$p" =~ $re ]]; then
+                prefix="${BASH_REMATCH[1]}" # extract what comes before 'import'
+                file="${BASH_REMATCH[2]}" # the file imported
+                suffix="${BASH_REMATCH[3]}" # what comes after
+                # Recompute path with new base
+                path="$(realpath --relative-to ./dist/ "$(dirname -- "$doc")/$file.typ")"
+                # Log substitution because this is a weak point, so this might break
+                >&2 echo "  path substitution: '$p' => '${prefix}import \"$path\"$suffix'"
+
+                # write
+                echo "${prefix}import \"$path\" $suffix" >> "$src"
+            else
+                # write
+                echo "$p" >> "$src"
+            fi
+        fi
+
+        if [[ "$p" == "//![FLASHBANG HEADER]"* ]]; then
+            # begin reading file
+            append=true
+        fi
+    done < "$doc"
+
+    echo "]" >> "$src"
+
     name="$(nameof "$doc")"
 
+    # Compile each file
     >&2 echo "compiling $doc -> ./dist/$name.pdf"
-
     typst compile --root . --font-path ./fonts --ignore-system-fonts "$doc" "./dist/$name.pdf"
 done
 
+# Compile merged file
+>&2 echo "compiling $src -> $full"
+typst compile --root . --font-path ./fonts --ignore-system-fonts "$src" "$full"
+
+# Build index
 cat > ./dist/index.html << EOF
 <html>
     <head>
@@ -53,13 +108,16 @@ cat > ./dist/index.html << EOF
     </head>
     <body>
         Flashbang cards
+        <a href="_full.pdf"> everything </a>
 EOF
 
+# Add links to individual files
 for doc in "${documents[@]}"; do
     name="$(nameof "$doc")"
     echo "<a href=\"$name.pdf\"> $name </a>" >> ./dist/index.html
 done
 
+# Finish index
 cat >> ./dist/index.html << EOF
     </body>
 </html>
