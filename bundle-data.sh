484c9ea3878a8869c2d208af9d457ada3bc8a2bf
viandoxdev <titouangoulois29@gmail.com>
2026-06-12 17:28:33 +0200

    I forgot

--- bundle-data.sh
+++ bundle-data.sh
@@ -2,7 +2,7 @@
 
 # This assumes we have fb-cli on hand, and that compile has been ran already.
 
-./fb-cli --search-path ./ --output-file ./public/src/lib/cards.json --output-dir ./cache/typst/repo --exclude ./cache
+./fb-cli --search-path ./ --output-file ./public/src/lib/cards.json --output-dir ./cache/typst/repo --exclude ./cache --with-search-text --typst-cache ./cache/typst/packages
 cd ./cache/typst/
 zip -r ../../public/static/data-bundle.zip ./repo ./packages
 cd -
